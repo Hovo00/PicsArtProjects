97568a90553105e0ab7a7db45497aef329e9957c
Hovo <hovik.grigoryan@picsart.com>
2022-11-17 15:57:32 +0400

    delete build file from git

--- build/CMakeFiles/expression_evaluator.dir/Token.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/expression_evaluator.dir/Token.cpp.o: \
-  /Users/hovogrigoryan/Documents/Picsart/PicsArt_projects/Token.cpp \
-  /Users/hovogrigoryan/Documents/Picsart/PicsArt_projects/Token.h
