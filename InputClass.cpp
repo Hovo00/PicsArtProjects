130824061262e475357a7654421497f6d6612374
Hovo <hovik.grigoryan@picsart.com>
2022-11-24 16:13:34 +0400

    clean files

--- InputClass.cpp
+++
@@ -1,9 +0,0 @@
-#include "InputClass.h"
-
-void ConsoleInput::getInput(std::string& inpStr){
-
-   std::cout << "Enter expression: ";
-   std::getline(std::cin, inpStr);
-
-   //std::cout << inpStr;
-}
--- Node.cpp
+++
@@ -1,4 +0,0 @@
-#include "Node.h"
-Node::Node(std::shared_ptr<Token> t, 
-    std::vector<std::shared_ptr<Node> > ch) : token(t), childs(ch) {
-}
